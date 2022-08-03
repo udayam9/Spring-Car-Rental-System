33c3a4a871a38a9d747211db3c11ef510f07bf81
udayam9 <udaya.madhushanka@gmail.com>
2022-08-04 02:39:34 +0530

    updated

--- src/main/java/lk/ijse/spring/service/CustomerService.java
+++ src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -3,6 +3,15 @@ package lk.ijse.spring.service;
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.entity.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
-    void saveCustomer(CustomerDTO customer);
+    void saveCustomer(CustomerDTO var1);
+    void updateCustomer(CustomerDTO var1);
+//    List<CarDTO> viewCars();
+//    void rentalRequest(RentalRequestDTO var1);
+    void existUserCustomerAccount(String var1);
+    void existEmail(String var1);
+    String getNewId();
+    void checkUserAccount(String var1, String var2);
 }
--- src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,10 +1,11 @@
-package lk.ijse.spring.service;
+package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.entity.CustomerUserAccount;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.repo.CustomerUserAccountRepo;
+import lk.ijse.spring.service.CustomerService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private CustomerRepo repo;
@@ -26,15 +27,15 @@ public class CustomerServiceImpl implements CustomerService{
 
     @Override
     public void saveCustomer(CustomerDTO customer) {
-        if (!repo.existsById(customer.getId())){
+        if (!repo.existsById(customer.getCustomerId())){
 
             repo.save(mapper.map(customer,Customer.class));
 
-            if (!customerUserAccountRepo.existsById(customer.getUserName())) {
+            if (!customerUserAccountRepo.existsById(customer.getCustomerUserAccount().getUserName())) {
                 //CustomerUserAccount customerUserAccount = new CustomerUserAccount(customer.getUserName(),customer.getPassword(),mapper.map(customer,Customer.class));
                 customerUserAccountRepo.save(mapper.map(customer,CustomerUserAccount.class));
             }else{
-                throw new RuntimeException("UserAccount Already Exit");
+                throw new RuntimeException("UserName Already Exit");
             }
 
         }else{
